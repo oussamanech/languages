a58463ae867307f771d661a54a6f2e54776942a8
oussamanech <oussamanach75@gmail.com>
2022-05-05 10:07:43 -0900

    Switch from Array to ArrayList

--- app/src/main/java/com/example/languages/NumbersActivity.java
+++ app/src/main/java/com/example/languages/NumbersActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
@@ -13,20 +15,32 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
         //create an array of words
-        String[] words = new String[10];
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
+//        String[] words = new String[10];
+//        words[0] = "one";
+//        words[1] = "two";
+//        words[2] = "three";
+//        words[3] = "four";
+//        words[4] = "five";
+//        words[5] = "six";
+//        words[6] = "seven";
+//        words[7] = "eight";
+//        words[8] = "nine";
+//        words[9] = "ten";
+
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
 
         for (int i = 0; i < 10; i++)
-            Log.v("NumbersActivity", "Word at index " + i + ": " + words[i]);
+            Log.v("NumbersActivity", "Word at index " + i + ": " + words.get(i));
 
     }
 }
