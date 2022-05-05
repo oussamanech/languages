5d006d5bcc411569526c0a19738a837a9d953378
oussamanech <oussamanach75@gmail.com>
2022-05-05 09:48:24 -0900

    Working with An Array

--- app/src/main/java/com/example/languages/NumbersActivity.java
+++ app/src/main/java/com/example/languages/NumbersActivity.java
@@ -3,6 +3,7 @@ package com.example.languages;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,5 +11,22 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //create an array of words
+        String[] words = new String[10];
+        words[0] = "one";
+        words[1] = "two";
+        words[2] = "three";
+        words[3] = "four";
+        words[4] = "five";
+        words[5] = "six";
+        words[6] = "seven";
+        words[7] = "eight";
+        words[8] = "nine";
+        words[9] = "ten";
+
+        for (int i = 0; i < 10; i++)
+            Log.v("NumbersActivity", "Word at index " + i + ": " + words[i]);
+
     }
 }
